f1aa880b0268a3b9176c822f8e3f2eb3f1f2a89a
janakalidasan T <janakalidasant@gmail.com>
2023-10-12 20:14:33 +0530

    first commit

---
+++ Assets/js/script.js
@@ -0,0 +1,45 @@
+fetch("./header.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
+
+fetch("./footer.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
+
+
+    // $(function() {
+    //     // Owl Carousel
+    //     var owl = $(".owl-carousel");
+    //     owl.owlCarousel({
+    //       items: 6,
+    //       margin: 10,
+    //       loop: true,
+    //       nav: true
+    //     });
+    //   });
+      
+    $(function() {
+        // Owl Carousel
+        var owl = $(".owl-carousel");
+        owl.owlCarousel({
+            items: 1,   // Display a single item on mobile
+            margin: 10,
+            loop: true,
+            nav: true,
+            responsive: {
+                // On larger screens, display more items
+                768: {
+                    items: 6
+                }
+            }
+        });
+    });
+    
